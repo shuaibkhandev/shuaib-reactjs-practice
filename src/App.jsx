bd8e9e558f85284fe75ab9aa5e7df1d3ffda37bd
shuaibkhan <skhan.csit@gmail.com>
2026-05-06 10:42:09 +0500

    installed React19 using vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MovieCard from "./components/MovieCard";
+
+const App = () => {
+  return (
+    <div>
+    <MovieCard/>
+    <MovieCard/>
+    <MovieCard/>
+    <MovieCard/>
+    </div>
+  );
+};
+
+export default App;
+
+
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,21 @@
+
+
+
+const MovieCard = () => {
+  return (
+      <div>
+      <div className="img-blog">
+        <img src="movie1-thumbnail.jpg" width={"250"} height={"250"} />
+      </div>
+      <h2>Name: 180</h2>
+      <h3>Rating: 8.2</h3>
+      <p>
+        Summary: When an unexpected road rage incident puts his son in critical
+        condition, an enraged father spirals down a dark path of emotional
+        turmoil and vengeance.
+      </p>
+    </div>
+  )
+}
+
+export default MovieCard;
